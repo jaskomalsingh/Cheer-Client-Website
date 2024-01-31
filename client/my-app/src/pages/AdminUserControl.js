29a4e83eadaf63f316ebac5f1f8cf97512292cb8
Ali Alibhai <Aalibh4@uwo.ca>
2024-01-31 15:57:46 -0500

    full name instead of username

--- client/my-app/src/pages/AdminUserControl.js
+++ client/my-app/src/pages/AdminUserControl.js
@@ -33,7 +33,8 @@ function AdminUserControl() {
         isDeactivated: user.isDeactivated,
         role: user.role,
         isVerified: user.isVerified,
-        isNews: user.isNews
+        isNews: user.isNews,
+        fullname: user.fullname
       }),
     })
       .then(response => {
@@ -57,7 +58,7 @@ function AdminUserControl() {
       <h2>User Management</h2>
       {users.map((user, index) => (
         <div key={user._id}>
-          <p onClick={() => handleExpandClick(user._id)}>{user.username} - {user.email}</p>
+          <p onClick={() => handleExpandClick(user._id)}>{user.fullname} - {user.email}</p>
           {expandedUserId === user._id && (
             <div>
               <p>
