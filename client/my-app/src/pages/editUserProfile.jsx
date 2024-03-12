ee4ce246a154593b48e99faf6783b3c7c91d6422
prabhb29 <prabhsimranb@hotmail.com>
2024-03-11 22:01:47 -0400

    fix footer and header in editUserProfile

--- client/my-app/src/pages/editUserProfile.jsx
+++ client/my-app/src/pages/editUserProfile.jsx
@@ -2,16 +2,20 @@ import React from "react";
 import LoginForm from "./LoginForm";
 import "../styles/style.css";
 import Header from "./Header";
-import { Footer } from "./Footer";
+import  Footer  from "./Footer";
 import EditProfileForm from "./EditProfileForm";
+import Container from 'react-bootstrap/Container';
 
 export const EditUserProfile = () => {
   return (
+    <Container fluid>
+    <Header/>
     <div className="edit-user-profile">
       <div className="div">
-        <Header/>
         <EditProfileForm className="login-form-instance" />
       </div>
     </div>
+    <Footer />
+    </Container>
   );
 };
