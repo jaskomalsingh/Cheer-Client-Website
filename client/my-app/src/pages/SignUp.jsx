04ffb944a43fcd68054997c36ceb9f7d9b26565e
prabhb29 <prabhsimranb@hotmail.com>
2024-03-11 22:47:49 -0400

    Fix header footer for signup

--- client/my-app/src/pages/SignUp.jsx
+++ client/my-app/src/pages/SignUp.jsx
@@ -3,6 +3,8 @@ import Header from "./Header";
 import Footer from "./Footer";
 import LoginForm from "./LoginForm";
 import "../styles/style.css";
+import Container from 'react-bootstrap/Container';
+import { useMediaQuery } from 'react-responsive';
 
 export const SignUp = () => {
   const [fullName, setFullName] = useState('');
@@ -18,7 +20,7 @@ export const SignUp = () => {
     // Data to be sent to the backend
     const signUpData = {
 
-     
+
     };
     body = JSON.stringify({
       fullname: fullName,
@@ -27,7 +29,7 @@ export const SignUp = () => {
       role: "user", // Assuming 'User' as a default role
       isNews: subscribeToNewsletter,
     })
-    
+
 
 
     try {
@@ -52,90 +54,94 @@ export const SignUp = () => {
     } catch (error) {
       console.error('Error during sign up:', error);
     }
-};
+  };
 
   return (
-    <div className="sign-up">
-      <div className="div">
-        <LoginForm className="login-form-instance" />
-        <div className="sign-up-form">
-          <div className="form-title">
-            <h2>Sign-Up</h2>
-            <p className="bio">Sign-up to create an account using your personal details.</p>
-          </div>
-          <div className="sign-up-form-content">
-            <div className="form-wrapper">
-              <div className="element-wrapper">
-                <label htmlFor="fullName" className="input-label">
-                  Full Name
-                </label>
-                <input
-                  type="text"
-                  id="fullName"
-                  value={fullName}
-                  placeholder='John Doe'
-                  onChange={(e) => setFullName(e.target.value)}
-                />
-              </div>
-              <div className="element-wrapper">
-                <label htmlFor="email" className="input-label">
-                  Email
-                </label>
-                <input
-                  type="email"
-                  id="email"
-                  value={email}
-                  placeholder='example@email.com'
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-              </div>
-            </div>
-            <div className="form-wrapper">
-              <div className="element-wrapper">
-                <label htmlFor="password" className="input-label">
-                  Password
-                </label>
-                <input
-                  type="password"
-                  id="password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-              </div>
-              <div className="element-wrapper">
-                <label htmlFor="confirmPassword" className="input-label">
-                  Confirm Password
-                </label>
-                <input
-                  type="password"
-                  id="confirmPassword"
-                  value={confirmPassword}
-                  onChange={(e) => setConfirmPassword(e.target.value)}
-                />
-              </div>
-            </div>
-            <div className="form-wrapper">
-            <div className="element-wrapper">
-              <label className="input-label" style={{ display: 'flex', alignItems: 'center' }}>
-              <input
-                  type="checkbox"
-                  id="subscribeToNewsletter"
-                  checked={subscribeToNewsletter}
-                  onChange={() => setSubscribeToNewsletter(!subscribeToNewsletter)}
-                  style={{ marginLeft: '8px' }}
-                />
-                Subscribe to Newsletter
-              </label>
-            </div>
-          </div>
-            <button type="button" className="button" onClick={handleSignUp}>
-              <div className="button-text">Sign Up</div>
-            </button>
-          </div>
-        </div>
-        <hr className="line" />
-      </div>
+    <Container fluid>
       <Header />
-    </div>
+      <div className="sign-up">
+        <div className="div">
+          <LoginForm className="login-form-instance" />
+          <div className="sign-up-form">
+            <div className="form-title">
+              <h2>Sign-Up</h2>
+              <p className="bio">Sign-up to create an account using your personal details.</p>
+            </div>
+            <div className="sign-up-form-content">
+              <div className="form-wrapper">
+                <div className="element-wrapper">
+                  <label htmlFor="fullName" className="input-label">
+                    Full Name
+                  </label>
+                  <input
+                    type="text"
+                    id="fullName"
+                    value={fullName}
+                    placeholder='John Doe'
+                    onChange={(e) => setFullName(e.target.value)}
+                  />
+                </div>
+                <div className="element-wrapper">
+                  <label htmlFor="email" className="input-label">
+                    Email
+                  </label>
+                  <input
+                    type="email"
+                    id="email"
+                    value={email}
+                    placeholder='example@email.com'
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </div>
+              </div>
+              <div className="form-wrapper">
+                <div className="element-wrapper">
+                  <label htmlFor="password" className="input-label">
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    id="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </div>
+                <div className="element-wrapper">
+                  <label htmlFor="confirmPassword" className="input-label">
+                    Confirm Password
+                  </label>
+                  <input
+                    type="password"
+                    id="confirmPassword"
+                    value={confirmPassword}
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                  />
+                </div>
+              </div>
+              <div className="form-wrapper">
+                <div className="element-wrapper">
+                  <label className="input-label" style={{ display: 'flex', alignItems: 'center' }}>
+                    <input
+                      type="checkbox"
+                      id="subscribeToNewsletter"
+                      checked={subscribeToNewsletter}
+                      onChange={() => setSubscribeToNewsletter(!subscribeToNewsletter)}
+                      style={{ marginLeft: '8px' }}
+                    />
+                    Subscribe to Newsletter
+                  </label>
+                </div>
+              </div>
+              <button type="button" className="button" onClick={handleSignUp}>
+                <div className="button-text">Sign Up</div>
+              </button>
+            </div>
+          </div>
+          <hr className="line" />
+        </div>
+
+      </div>
+      <Footer />
+    </Container>
   );
 };
